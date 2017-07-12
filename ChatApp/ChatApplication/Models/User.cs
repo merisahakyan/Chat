4fac020820e113b3f675249929c70731d91153f4
merisahakyan <sahakyan_m@bk.ru>
2017-07-12 14:58:52 +0400

    chat

---
+++ ChatApp/ChatApplication/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatApplication.Models
+{
+    public class User
+    {
+        public string ConnectionId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ChatApp/ChatApplication/Scripts/util.js
@@ -0,0 +1,78 @@
+﻿$(function () {
+
+    $('#chatBody').hide();
+    $('#loginBlock').show();
+    var chat = $.connection.chatHub;
+
+    chat.client.addMessage = function (name, message) {
+
+        $('#chatroom').append('<p><b>' + htmlEncode(name)
+            + '</b>: ' + htmlEncode(message) + '</p>');
+    };
+
+
+    chat.client.onConnected = function (id, userName, allUsers) {
+
+        $('#loginBlock').hide();
+        $('#chatBody').show();
+
+        $('#hdId').val(id);
+        $('#username').val(userName);
+        $('#header').html('<h3>Welcome, ' + userName + '</h3>');
+
+
+        for (var i = 0; i < allUsers.length; i++) {
+
+            AddUser(allUsers[i].ConnectionId, allUsers[i].Name);
+        }
+    }
+
+
+    chat.client.onNewUserConnected = function (id, name) {
+
+        AddUser(id, name);
+    }
+
+
+    chat.client.onUserDisconnected = function (id, userName) {
+
+        $('#' + id).remove();
+    }
+
+
+    $.connection.hub.start().done(function () {
+
+        $('#sendmessage').click(function () {
+
+            chat.server.send($('#username').val(), $('#message').val());
+            $('#message').val('');
+        });
+
+
+        $("#btnLogin").click(function () {
+
+            var name = $("#txtUserName").val();
+            if (name.length > 0) {
+                chat.server.connect(name);
+            }
+            else {
+                alert("Enter name..!!");
+            }
+        });
+    });
+});
+
+function htmlEncode(value) {
+    var encodedValue = $('<div />').text(value).html();
+    return encodedValue;
+}
+
+function AddUser(id, name) {
+
+    var userId = $('#hdId').val();
+
+    if (userId != id) {
+
+        $("#chatusers").append('<p id="' + id + '"><b>' + name + '</b></p>');
+    }
+}

28b37bf470be0d7f0ad0a2dd1ec9a26cb6ef0884
merisahakyan <sahakyan_m@bk.ru>
2017-07-13 10:09:57 +0400

    view

--- ChatApp/ChatApplication/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChatApplication.MvcApplication" Language="C#" %>
--- Chat_With_SignalR/Simple_Chat/Scripts/util.js
+++ Chat_With_SignalR/Simple_Chat/Scripts/util.js
@@ -3,52 +3,53 @@
     $('#chatBody').hide();
     $('#loginBlock').show();
     var chat = $.connection.chatHub;
-    
+
     chat.client.addMessage = function (name, message) {
-    
+
         $('#chatroom').append('<p><b>' + name
             + '</b>: ' + message + '</p>');
+       
     };
 
-    
+
     chat.client.onConnected = function (id, userName, allUsers) {
 
         $('#loginBlock').hide();
         $('#chatBody').show();
-    
+
         $('#hdId').val(id);
         $('#username').val(userName);
         $('#header').html('<h3>Welcome, ' + userName + '</h3>');
 
-    
+
         for (var i = 0; i < allUsers.length; i++) {
 
             AddUser(allUsers[i].ConnectionId, allUsers[i].Name);
         }
     }
 
-    
+
     chat.client.onNewUserConnected = function (id, name) {
 
         AddUser(id, name);
     }
 
-    
+
     chat.client.onUserDisconnected = function (id, userName) {
 
         $('#' + id).remove();
     }
 
-    
+
     $.connection.hub.start().done(function () {
 
         $('#sendmessage').click(function () {
-    
+
             chat.server.send($('#username').val(), $('#message').val());
             $('#message').val('');
         });
 
-    
+
         $("#btnLogin").click(function () {
 
             var name = $("#txtUserName").val();

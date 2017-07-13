4273b30ed042cc5a0b96400d2cb32001e341f1f6
sahakyan.meri <sahakyan.meri@F4FS-01132>
2017-07-13 17:50:21 +0400

    .

--- Chat_With_SignalR/Simple_Chat/Scripts/util.js
+++ Chat_With_SignalR/Simple_Chat/Scripts/util.js
@@ -2,15 +2,8 @@
 
     $('#chatBody').hide();
     $('#loginBlock').show();
+
     var chat = $.connection.chatHub;
-
-    chat.client.addMessage = function (name, message) {
-
-        $('#chatroom').append('<p><b>' + name
-            + '</b>: ' + message + '</p>');
-       
-    };
-
 
     chat.client.onConnected = function (id, userName, allUsers) {
 
@@ -27,18 +20,37 @@
             AddUser(allUsers[i].ConnectionId, allUsers[i].Name);
         }
     }
-
-
     chat.client.onNewUserConnected = function (id, name) {
 
         AddUser(id, name);
     }
-
-
     chat.client.onUserDisconnected = function (id, userName) {
 
         $('#' + id).remove();
     }
+    chat.client.addMessage = function (name, message) {
+        if ($('#message').val() != '') {
+            $('#chatroom').append('<p><b>' + name
+                + '</b>: ' + message + '</p>');
+            $("#chatroom").scrollTop($("#chatroom")[0].scrollHeight);
+        }
+    };
+
+    $(document).ready(function () {
+        $('#txtUserName').keypress(function (event) {
+
+            if (event.which == 13) {
+                var name = $("#txtUserName").val();
+                if (name.length > 0) {
+                    chat.server.connect(name);
+
+                }
+                else {
+                    alert("Enter name..!!");
+                }
+            }
+        });
+    });
 
 
     $.connection.hub.start().done(function () {
@@ -49,16 +61,27 @@
             $('#message').val('');
         });
 
+        $(document).ready(function () {
+            $('#message').keypress(function (event) {
+
+                if (event.which == 13 && $('#message').val() != '') {
+                    AddMessage($('#username').val(), $('#message').val());
+                    $('#message').val('');
+                }
+            });
+        });
 
         $("#btnLogin").click(function () {
 
             var name = $("#txtUserName").val();
             if (name.length > 0) {
                 chat.server.connect(name);
+                
             }
             else {
                 alert("Enter name..!!");
             }
+
         });
     });
 });
@@ -72,5 +95,25 @@ function AddUser(id, name) {
     if (userId != id) {
 
         $("#chatusers").append('<p id="' + id + '"><b>' + name + '</b></p>');
+    }
+}
+function AddMessage(name, message) {
+    $('#chatroom').append('<p><b>' + name
+        + '</b>: ' + message + '</p>');
+    $("#chatroom").scrollTop($("#chatroom")[0].scrollHeight);
+};
+function OnConnected(id, userName, allUsers) {
+
+    $('#loginBlock').hide();
+    $('#chatBody').show();
+
+    $('#hdId').val(id);
+    $('#username').val(userName);
+    $('#header').html('<h3>Welcome, ' + userName + '</h3>');
+
+
+    for (var i = 0; i < allUsers.length; i++) {
+
+        AddUser(allUsers[i].ConnectionId, allUsers[i].Name);
     }
 }

9c74e7faefd0a0b83ac4579ff0c124d5271c827c
merisahakyan <sahakyan_m@bk.ru>
2017-07-25 17:49:05 +0400

    changes

--- Chat_With_SignalR/Simple_Chat/Hubs/ChatHub.cs
+++ Chat_With_SignalR/Simple_Chat/Hubs/ChatHub.cs
@@ -70,47 +70,16 @@
                     login = false;
                 }
             }
-            //else if (ActiveUsers.Where(p => p.Name == username).Count() == 1 && roomname == "general")
-            //{
-            //    ActiveUsers.Where(p => p.Name == uname).First().ConnectionId = id;
-            //    var rooms = _context.Rooms;
-            //    List<Room> nr = new List<Room>();
-            //    foreach (var m in rooms)
-            //    {
-            //        if (m.RoomName == "general")
-            //            continue;
-            //        Room r = new Room(m);
-            //        nr.Add(r);
-            //    }
-            //    Clients.Client(id).onConnected(id, username, ActiveUsers, nr);
-            //}
-
         }
         public void Send(string name, string message)
         {
-
-            //_context.Messages.Add(new Simple_Chat.Message()
-            //{
-            //    MessageText = message,
-            //    User = _context.Users.FirstOrDefault(p => p.UserName == name),
-            //    Room = _context.Rooms.FirstOrDefault(p => p.RoomName == "general")
-            //});
-            //_context.SaveChanges();
             manager.InsertMessage(name, "general", message);
             Clients.All.addMessage(name, message);
         }
         public void SendToGroup(string username, string message, string roomname)
         {
-            //_context.Messages.Add(new Simple_Chat.Message()
-            //{
-            //    MessageText = message,
-            //    User = _context.Users.FirstOrDefault(p => p.UserName == username),
-            //    Room = _context.Rooms.FirstOrDefault(p => p.RoomName == roomname)
-            //});
-            //_context.SaveChanges();
             manager.InsertMessage(username, roomname, message);
             Clients.All.addMessageToRoom(username, message, roomname);
-            //var usersfornotify = _context.Rooms.FirstOrDefault(p => p.RoomName == roomname).Users;
             var usersfornotify = manager.GetUsersByRoom(roomname);
             List<string> notify = new List<string>();
             foreach (var m in usersfornotify)
@@ -126,12 +95,8 @@
             var id = Context.ConnectionId;
             uname = username;
             login = true;
-            // if (ActiveUsers.All(x => x.ConnectionId != id) && ActiveUsers.All(x => x.Name != username) && _context.Users.Where(p => p.UserName == username).Count() > 0)
             if (ActiveUsers.All(x => x.ConnectionId != id) && ActiveUsers.All(x => x.Name != username))
             {
-                //var user = _context.Users.Where(p => p.UserName == username).First();
-                //if (user.Password == password && user.active)
-
                 if (manager.Login(username, password) && manager.GetUserByName(username).active)
                 {
                     ActiveUsers.Add(new User() { ConnectionId = id, Name = username, Password = password });
@@ -158,12 +123,8 @@
         }
         public void Create(string group)
         {
-            // if (_context.Rooms.Where(p => p.RoomName == group).Count() == 0)
             if (manager.IsValidRoomName(group))
             {
-                //var id = Context.ConnectionId;
-                //_context.Rooms.Add(new Simple_Chat.Room() { RoomName = group });
-                //_context.SaveChanges();
                 manager.InsertRoom(group);
                 Clients.All.onNewGroupCreating(group);
             }
@@ -174,26 +135,17 @@
         }
         public void JoinGroup(string username, string roomname)
         {
-            //Simple_Chat.User user = _context.Users.FirstOrDefault(p => p.UserName == username);
-            //_context.Rooms.FirstOrDefault(p => p.RoomName == roomname).Users.Add(user);
-            //_context.SaveChanges();
             manager.JoinGroup(username, roomname);
             condition = "join";
         }
         public void OutFromRoom(string username, string roomname)
         {
-            //Simple_Chat.User user = _context.Users.FirstOrDefault(p => p.UserName == username);
-            //_context.Rooms.FirstOrDefault(p => p.RoomName == roomname).Users.Remove(user);
-            //_context.SaveChanges();
             manager.OutFromRoom(username, roomname);
             Clients.All.outFromRoom(username);
             Clients.All.onRoomOut(username, roomname);
         }
         public void OutButton(string username, string roomname)
         {
-            //Simple_Chat.User user = _context.Users.FirstOrDefault(p => p.UserName == username);
-            //_context.Rooms.FirstOrDefault(p => p.RoomName == roomname).Users.Remove(user);
-            //_context.SaveChanges();
             manager.OutFromRoom(username, roomname);
             Clients.Caller.outRoomButton(roomname);
             Clients.All.onRoomOut(username, roomname);
@@ -206,15 +158,11 @@
         {
             string tok = Guid.NewGuid().ToString();
             bool t;
-            //if (_context.Users.Where(p => p.UserName == username).Count() == 0 && _context.Users.Where(p => p.eMail == email).Count() == 0 && username.Length < 20 && password.Length >= 6)
             if (manager.IsValidNameOrEmail(username, email) && username.Length < 20 && password.Length >= 6)
             {
                 MailProvider.SendMail(email, tok);
-                //Simple_Chat.User user = new Simple_Chat.User() { active = false, eMail = email, Password = password, UserName = username, token = tok };
                 manager.InsertUser(username, email, password, tok, false);
-                // _context.Users.Add(user);
                 t = true;
-                // _context.SaveChanges();
             }
             else
             {
